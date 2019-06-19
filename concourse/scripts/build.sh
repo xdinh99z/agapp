21a9d742f7822beda2e8889d6eb0b9b6bd61cc37
Dinh <xdinh@dxc.com>
2019-06-19 10:39:30 +0700

    Update status file

--- concourse/scripts/build.sh
+++ concourse/scripts/build.sh
@@ -8,9 +8,11 @@
 #mkdir -p $DIST_FOLDER
 cd agapp
 
+npm config set registry https://registry.npmjs.org/
 npm install --ignore-scripts
 npm install -g @angular/cli > /dev/null
 npm run build --prod
+cp Staticfile dist/agapp
 
 #cd dist
 
